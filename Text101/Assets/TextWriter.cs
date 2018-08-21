3078cdcd4f7657ccab26159e57b5736bc92f29b9
pxdavidson <38045508+pxdavidson@users.noreply.github.com>
2018-08-21 17:55:19 +0100

    T101_2A
    ##CODE
    +TextWriter.cs
    
    ##ASSETS
    +Game Script Depot object to hold scripts

---
+++ Text101/Assets/TextWriter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriter : MonoBehaviour
+{
+
+    // Define externals
+    [SerializeField] Text bodyTextComponent;
+
+    // Define Variables
+    string bodyTextContent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SetIntroText();
+        bodyTextComponent.text = bodyTextContent;
+	}
+
+    // Sets the introduction text
+    void SetIntroText()
+    {
+        bodyTextContent = "Hello World";
+    }
+
+    // Call this and pass updated text to it to display
+    public void UpdateText(string updatedText)
+    {
+        bodyTextContent = updatedText;
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
